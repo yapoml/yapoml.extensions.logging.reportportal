5a81de6827c072913f2ba502b1a5f42bf50b311c
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-06-28 12:40:23 +0300

    Added reportportal adapter for logs

---
+++ src/Yapoml.Extensions.Logging.ReportPortal/ReportPortalAdapter.cs
@@ -0,0 +1,10 @@
+﻿namespace Yapoml.Extensions.Logging.Serilog
+{
+    public class ReportPortalAdapter : Framework.Logging.ILogger
+    {
+        public void Trace(string message)
+        {
+            ReportPortal.Shared.Context.Current.Log.Trace(message);
+        }
+    }
+}
---
+++ src/Yapoml.Extensions.Logging.ReportPortal/SpaceOptionsExtension.cs
@@ -0,0 +1,15 @@
+﻿using Yapoml.Extensions.Logging.Serilog;
+using Yapoml.Framework.Options;
+
+namespace Yapoml
+{
+    public static class SpaceOptionsExtension
+    {
+        public static ISpaceOptions UseReportPortal(this ISpaceOptions spaceOptions)
+        {
+            spaceOptions.Services.Register<Framework.Logging.ILogger>(new ReportPortalAdapter());
+
+            return spaceOptions;
+        }
+    }
+}
